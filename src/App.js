3d61f06725bbab221908bb69dba441eb2350929e
Ignacio Contreres Kirkby <contreresignacio@gmail.com>
2022-12-08 00:44:13 -0300

    added initial routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Characters from "./features/characters";
 
 const App = () => {
   return (
-    <>
-      <div>Star wars app!</div>
-    </>
+    <Router>
+      {/* <Menu/> */}
+      <Switch>
+        <Route exact path="/" component={Characters} />
+      </Switch>
+    </Router>
   );
 };
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Routing />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 export default App;

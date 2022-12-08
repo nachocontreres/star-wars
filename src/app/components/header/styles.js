bfab6522b33b6184c743bbb7f10fa9a7e7f2827f
Ignacio Contreres Kirkby <contreresignacio@gmail.com>
2022-12-08 20:38:20 -0300

    created header component containing burger and menu components

---
+++ src/app/components/header/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledPageHeader = styled.h1`
+  position: absolute;
+  top: 22px;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 15px;
+  font-weight: 600;
+  text-align: center;
+`;

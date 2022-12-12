060aa867f44a4d7c6693d1e13d2068c58ebef822
Ignacio Contreres Kirkby <contreresignacio@gmail.com>
2022-12-12 17:07:57 -0300

    Updates

--- src/features/characters/index.test.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = "https://swapi.dev/api/";
-
-test("On mount, get characters should retrieve values", async () => {
-  const response = await axios.get("people/?page=1");
-  expect(response.data.results.length).toBeGreaterThan(0);
-});
